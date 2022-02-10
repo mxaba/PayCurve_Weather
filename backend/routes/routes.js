67c7ed81aac8c39834d881c99606337ab518710a
Mcebo Samuel <samuel.mcebo@gmail.com>
2022-02-10 15:13:42 +0200

    getting the user when login

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -10,8 +10,24 @@ router.post('/register', async(req, res) => {
         name: req.body.name,
         email: req.body.email,
         password: hashedPassword,
-    })
-    res.send(await user.save());
+    });
+    const results = await user.save();
+    const {password, ...data} = await results.toJSON();
+    res.send(data);
+});
+
+router.post('/login', async(req, res) => {
+    const user = await User.findOne({email: req.body.email});
+
+    if(!user) {
+        return res.status(404).send({message: "user not found"});
+    }
+
+    if(!await bcrypt.compare(req.body.password, user.password)){
+        return res.status(404).send({message: "Password is incorrect"});
+    }
+
+    res.send(user);
 });
 
 module.exports = router;
