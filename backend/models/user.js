97823b286e9cde9408964b4fe34ad4b497253d59
Mcebo Samuel <samuel.mcebo@gmail.com>
2022-02-10 13:55:33 +0200

    created connection of D

--- backend/models/user.js
+++ backend/models/user.js
@@ -14,6 +14,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         require: true,
     }
-})
+});
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
