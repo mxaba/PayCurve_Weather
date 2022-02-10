6f9e3d7b568dd2fe4c6073256a01913bb6e623e4
Mcebo Samuel <samuel.mcebo@gmail.com>
2022-02-10 13:55:07 +0200

    created connection of DB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-app = express();
+const mongodbURL = "mongodb+srv://PayCurve_Wether_App:PayCurve_Wether_App@paycurve-weather-app.2lr9s.mongodb.net/PayCurveWeatherApp?retryWrites=true&w=majority";
+mongoose.connect(
+    mongodbURL,
+    {
+   useNewUrlParser: true
+    }
+).then(console.log("Connected to the database")).catch((err) => console.log(err));
+
+
+let app = express();
 
 app.get('/', (req, res) => {
     res.send('Hello');
